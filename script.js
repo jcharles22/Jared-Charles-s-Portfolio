f2cbde41e3a645b4bee09a627ba288d48d5f9aab
Jared Charles <jaredcharles22@gmail.com>
2019-03-24 11:04:35 -0400

    commeted out console.log

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ function windowSize(){
     resizeWindow();
     $(window).resize(function(){     
     resizeWindow();   
-});
+    });
+}
 
 function resizeWindow(){
     let winWidth = $(window).width();
-    console.log(winWidth);
+    // console.log(winWidth);
     if(winWidth < 1265) {
         $('.footer').insertAfter($('.content'));
         } else if (winWidth > 1266) {
             $('.footer').appendTo($('.hero'));
         }
     };
-}
+
 $(windowSize);
 
 
