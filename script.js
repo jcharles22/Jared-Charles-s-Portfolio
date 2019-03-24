76f90f70f6fdda6b3a50f0b55a08326617c2fb90
Jared Charles <jaredcharles22@gmail.com>
2019-03-23 20:13:16 -0400

    added js css responsive

---
+++ script.js
@@ -0,0 +1,19 @@
+function windowSize(){
+    resizeWindow();
+    $(window).resize(function(){     
+    resizeWindow();   
+});
+
+function resizeWindow(){
+    let winWidth = $(window).width();
+    console.log(winWidth);
+    if(winWidth < 1265) {
+        $('.footer').insertAfter($('.content'));
+        } else if (winWidth >= 1265) {
+            $('.footer').appendTo($('.hero'));
+        }
+    };
+}
+$(windowSize);
+
+
